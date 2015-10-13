de272e333862f26b10670d041e791607dc1b28e8
Andreas Bergh <andreas__bergh@hotmail.com>
2015-10-13 08:19:49 +0000

    Fixed JQuery mobile scroll

--- js/custom.js
+++ js/custom.js
@@ -22,10 +22,10 @@ $(this).ready(function() {
     //KONTROLLERAR OM SIDAN ÄR I "TOP" ANNARS VISA KNAPP TILLBAKA
     $(window).scroll(function() {
         if ($(this).scrollTop() > amountScrolled) {
-            $('a.back-to-top').fadeIn();//FADAR IN KNAPPEN
+            $('a.back-to-top').fadeIn(); //FADAR IN KNAPPEN
         }
         else {
-            $('a.back-to-top').fadeOut();//FADAR UT KNAPPEN
+            $('a.back-to-top').fadeOut(); //FADAR UT KNAPPEN
         }
     });
     $('a.back-to-top').click(function() { // SCROLLAR TILL TOPPEN VID KNAPPTRYCKNING
@@ -41,7 +41,7 @@ $(this).ready(function() {
 var didScroll;
 var lastScrollTop = 0;
 var downDelta = 200;
-var navbarHeight = $('.nav').outerHeight();
+var navbarHeight = $('header, nav').outerHeight();
 $(window).scroll(function(event) {
     didScroll = true;
 });
@@ -73,6 +73,37 @@ function hasScrolled() {
 }
 
 
+/* WEBFUNKTION*/
+$(function() {
+    // HÄMTAR TOP OFFSET I MENYN
+    var stickyNavTop = $('header, nav').offset().top;
+    // BESTÄMMER OM NAV ÄR FAST ELLER INTE
+    var stickyNav = function() {
+        var scroll_top = $(window).scrollTop(); // POSITON TILL SIDANS TOPP
+        // OM SCROLLING SKER LÄNGER ÄN TILL NAV, FÄST NAV -50PX,
+        if (scroll_top > stickyNavTop) {
+            $('header, nav').css({
+                'position': 'fixed',
+                'top': -50,
+                'left': 0,
+            });
+        }
+        else {
+            $('header, nav').css({
+                'position': 'absolute',
+                'top': 0,
+            });
+        }
+    };
+    // STARTAR FUNKTIONEN
+    stickyNav();
+
+    // OCH KÖR VID VARJE SCROLLNING
+    $(window).scroll(function() {
+        stickyNav();
+    });
+});
+
 /*
 =============================================
 MOBILE JQUERY 
@@ -81,31 +112,35 @@ MOBILE JQUERY
 
 /* MOBILMENY KNAPP */
 $('button.mob-button').click(
-function () {
-    $('.mobile').slideToggle(1000)('slideToggle');
-});
+    function() {
+        $('.mobile').slideToggle(1000)('slideToggle');
+    });
 
-$('.mob-sub').click(
-function () {
-    $(this).parent().toggleClass("slideToggle");
-});
-
+/*$('.mob-sub').click(
+    function() {
+        $(this).parent().toggleClass('slideToggle');
+    });
+*/
+/* MOBILEVERSION*/
 $(function() {
     // HÄMTAR TOP OFFSET I MENYN
-    var stickyNavTop = $('header,nav').offset().top;
+    var stickyNavTop = $('header.mob-header, .mobile').offset().top;
     // BESTÄMMER OM NAV ÄR FAST ELLER INTE
     var stickyNav = function() {
         var scroll_top = $(window).scrollTop(); // POSITON TILL SIDANS TOPP
-        // OM SCROLLING SKER LÄNGER ÄN TILL NAV, FÄST NAV -50PX,
+        // OM SCROLLING SKER LÄNGER ÄN TILL NAV, FÄST NAV -400PX,
         if (scroll_top > stickyNavTop) {
-            $('header,nav').css({
+            $('header.mob-header, .mobile').css({
                 'position': 'fixed',
-                'top': -50,
+                'top': -400,
                 'left': 0,
+
             });
+            $('.mobile').toggleClass('slideToggle').hide();
+            
         }
         else {
-            $('header,nav').css({
+            $('header.mob-header, .mobile').css({
                 'position': 'absolute',
                 'top': 0,
             });
