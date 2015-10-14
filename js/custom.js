f4e820c9409f8bad7fd96377403161ab5e6fcba7
Andreas Bergh <andreas__bergh@hotmail.com>
2015-10-14 17:04:01 +0000

    Fix & trix css

--- js/custom.js
+++ js/custom.js
@@ -156,3 +156,11 @@ $(function() {
 });
 */
 
+$(function(){
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+      $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');}, 
+      3000);
+});
